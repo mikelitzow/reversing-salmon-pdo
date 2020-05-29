73b6b0d1d17fd374f497034a2e19a1b0ca4672fc
mike.litzow <mike.litzow@noaa.gov>
2020-05-28 18:42:12 -0800

    cleaning up

--- scripts/PDO and climate.R
+++ scripts/PDO and climate.R
@@ -13,27 +13,46 @@ library(MARSS)
 library(overlapping)
 library(RNetCDF)
 
-dat <- read.csv("data/climate.data.csv", row.names = 1)
+dat <- read.csv("data/climate.data.csv")
 cb <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 
 # excluding variables showing negative results (these are plotted separately below)
+# and limit to 1965-onwards
 dat <- dat %>%
-  select(-Papa, -GAK1.sal)
+  select(-Papa, -GAK1.sal, -NDJFM.PDO) %>%
+  filter(year > 1964)
+
+# add NCDC PDO values
+dat2 <- read.csv("data/salmon.and.NCDC.PDO.csv")
+
+dat2 <- dat2 %>%
+  group_by(Year) %>%
+  summarise(NDJFM.PDO=mean(PDO1))
+
+names(dat2)[1] <- "year"
+
+dat <- left_join(dat, dat2)
+
+# and replace the ssh coastal mean with ssh PC1
+dat3 <- read.csv("data/salmon.covariates.updated.ssh.eof.csv")
+dat$FMA.SSH.PC1 <- dat3$FMA.ssh.PC1.SODA.pred.by.GODAS[match(dat$year, dat3$year)]
 
 # now plot relative to PDO!
-colnames(dat)[c(2,3,5,6)] <- c("Sea surface height (m)",
+colnames(dat)[c(7,4,5,6)] <- c("Sea surface height (PC1)",
                                "Wind stress (pa)",
                                "Sea surface temp. (ºC)",
                              "North Pacific Index (mb)")
-dat <- dat %>%
-  gather(key, value, -NDJFM.PDO, -era)
 
-dat$plot.era <- ifelse(dat$era==1, "1964-1988",
+dat <- dat %>%
+  select(-year, -FMA.ssh.COMBINED) %>%
+  pivot_longer(cols=c(-NDJFM.PDO, -era), names_to = 'key')
+
+dat$plot.era <- ifelse(dat$era==1, "1965-1988",
                        ifelse(dat$era==2, "1989-2013", "2014-2019"))
 
 dat$key.order <- ifelse(dat$key=="North Pacific Index (mb)", 1,
                         ifelse(dat$key=="Sea surface temp. (ºC)", 2,
-                        ifelse(dat$key=="Sea surface height (m)", 3, 4)))
+                        ifelse(dat$key=="Sea surface height (PC1)", 3, 4)))
 
 dat$key <- reorder(dat$key, dat$key.order)
 
@@ -46,7 +65,7 @@ scatter <- ggplot(dat, aes(NDJFM.PDO, value, color=plot.era)) +
   xlab("PDO Index (Nov-Mar)") +
   theme(legend.title = element_blank(), axis.title.y = element_blank(), legend.position = 'top')
 
-ggsave("PDO vs NPI ssh sst stress.png", width = 5, height = 5, units="in")
+ggsave("figs/PDO vs NPI ssh sst stress updated with stress PC1.png", width = 5, height = 5, units="in")
 
 # and try with stan models
 library(rstan)
@@ -133,7 +152,7 @@ for(i in 1:length(unique(coef_indv_arm$key))) {
   sub = dplyr::filter(coef_indv_arm, key == unique(coef_indv_arm$key)[i])
   # calculate pairwise overlaps in slopes and intercepts
   int_overlap = overlapping::overlap(x = list(int1 = sub$era1,int2=sub$era2,int3=sub$era3))
-  saveRDS(int_overlap$OV,file=paste0(sub$key[1], "_climate_int_overlap.rds"))
+  saveRDS(int_overlap$OV,file=paste0("output/", sub$key[1], "_climate_int_overlap.rds"))
 
 }
 
@@ -156,7 +175,7 @@ for(i in 1:length(unique(coef_slope$key))) {
   sub = dplyr::filter(coef_slope, key == unique(coef_slope$key)[i])
   # calculate pairwise overlaps in slopes and intercepts
   int_overlap = overlapping::overlap(x = list(slope1 = sub$era1,slope2=sub$era2,slope3=sub$era3))
-  saveRDS(int_overlap$OV,file=paste0(sub$key[1], "_climate_slope_overlap.rds"))
+  saveRDS(int_overlap$OV,file=paste0("output/", sub$key[1], "_climate_slope_overlap.rds"))
   
 
 }
@@ -172,9 +191,38 @@ slope_tab <- plyr::ddply(mdf_slope, .(key, variable), summarize,
                          upper95 = quantile(value, probs = 0.975))
 
 
-write.csv(int_tab, "climate intercept table.csv")
+# get intercept / slope differences in units of SD
+int_tab[3,3]-int_tab[1,3] # NPI era3 - era1
 
-write.csv(slope_tab, "climate slope table.csv")
+
+slope_tab[3,3]-slope_tab[1,3] # NPI era3 - era1
+slope_tab[3,3]-slope_tab[2,3] # NPI era3 - era2
+
+slope_tab[6,3]-slope_tab[4,3] # sst era3 - era1
+slope_tab[6,3]-slope_tab[5,3] # sst era3 - era2
+
+slope_tab[9,3]-slope_tab[7,3] # ssh era3 - era1
+slope_tab[9,3]-slope_tab[8,3] # ssh era3 - era2
+
+slope_tab[12,3]-slope_tab[10,3] # stress era3 - era1
+slope_tab[12,3]-slope_tab[11,3] # stress era3 - era2
+
+
+int_tab[3,3]-int_tab[1,3] # NPI era3 - era1
+int_tab[3,3]-int_tab[2,3] # NPI era3 - era2
+
+int_tab[6,3]-int_tab[4,3] # sst era3 - era1
+int_tab[6,3]-int_tab[5,3] # sst era3 - era2
+
+int_tab[9,3]-int_tab[7,3] # ssh era3 - era1
+int_tab[9,3]-int_tab[8,3] # ssh era3 - era2
+
+int_tab[12,3]-int_tab[10,3] # stress era3 - era1
+int_tab[12,3]-int_tab[11,3] # stress era3 - era2
+
+write.csv(int_tab, "output/climate intercept table.csv")
+
+write.csv(slope_tab, "output/climate slope table.csv")
 
 int <- ggplot(mdf_indv_arm, aes(x = value, fill = variable)) +
   theme_bw() +
@@ -190,7 +238,9 @@ print(int)
 # make a combined plot
 library(ggpubr)
 
-png("era-specific PDO and climate.png", 8, 3.75, units='in', res=300)
+png("figs/era-specific PDO and climate updated.png", 8, 3.75, units='in', res=300)
+
+tiff("figs/era-specific PDO and climate updated.tiff", 8, 3.75, units='in', res=300)
 ggarrange(scatter, int, ncol=2, nrow=1, labels=c("a)", "b)"),
           label.x = 0.05, label.y = 0.95)
 dev.off()
@@ -207,7 +257,7 @@ slope <- ggplot(mdf_slope, aes(x = value, fill = variable)) +
   facet_wrap( ~ key, scales="free")
 print(slope)
 
-ggsave("era-specific climate slopes on PDO.png", width=6, height=4, units='in')
+ggsave("figs/era-specific climate slopes on PDO updated.png", width=6, height=4, units='in')
 
 ## Bayesian model diagnostics
 post_era_NPI_2    <- as.array(era_NPI_2)
@@ -315,7 +365,7 @@ for(i in 1:length(unique(coef_indv_arm$key))) {
   sub = dplyr::filter(coef_indv_arm, key == unique(coef_indv_arm$key)[i])
   # calculate pairwise overlaps in slopes and intercepts
   int_overlap = overlapping::overlap(x = list(int1 = sub$era1,int2=sub$era2,int3=sub$era3))
-  saveRDS(int_overlap$OV,file=paste0(sub$key[1], "_climate_int_overlap.rds"))
+  saveRDS(int_overlap$OV,file=paste0("output/", sub$key[1], "_climate_int_overlap.rds"))
 }
 
 ## extract slopes
@@ -336,7 +386,7 @@ for(i in 1:length(unique(coef_slope$key))) {
   sub = dplyr::filter(coef_slope, key == unique(coef_slope$key)[i])
   # calculate pairwise overlaps in slopes and intercepts
   int_overlap = overlapping::overlap(x = list(slope1 = sub$era1,slope2=sub$era2,slope3=sub$era3))
-  saveRDS(int_overlap$OV,file=paste0(sub$key[1], "_climate_slope_overlap.rds"))
+  saveRDS(int_overlap$OV,file=paste0("output/", sub$key[1], "_climate_slope_overlap.rds"))
 }
 
 int_tab_neg <- plyr::ddply(mdf_indv_arm, .(key, variable), summarize,
@@ -348,6 +398,18 @@ slope_tab_neg <- plyr::ddply(mdf_slope, .(key, variable), summarize,
                          mean = mean(value),
                          lower95 = quantile(value, probs = 0.025),
                          upper95 = quantile(value, probs = 0.975))
+
+int_tab_neg[3,3]-int_tab_neg[1,3] # gak1 era3 - era1
+int_tab_neg[3,3]-int_tab_neg[2,3] # gak1 era3 - era2
+
+int_tab_neg[6,3]-int_tab_neg[4,3] # Papa era3 - era1
+int_tab_neg[6,3]-int_tab_neg[5,3] # Papa era3 - era2
+
+slope_tab_neg[3,3]-slope_tab_neg[1,3] # gak1 era3 - era1
+slope_tab_neg[3,3]-slope_tab_neg[2,3] # gak1 era3 - era2
+
+slope_tab_neg[6,3]-slope_tab_neg[4,3] # Papa era3 - era1
+slope_tab_neg[6,3]-slope_tab_neg[5,3] # Papa era3 - era2
 
 # plot intercepts
 int <- ggplot(mdf_indv_arm, aes(x = value, fill = variable)) +
@@ -374,7 +436,7 @@ slope <- ggplot(mdf_slope, aes(x = value, fill = variable)) +
 print(slope)
 
 # make a combined plot
-png("era-specific PDO and GAK1-Papa.png", 6.5, 7, units='in', res=300)
+png("figs/SI - era-specific PDO and GAK1-Papa.png", 6.5, 7, units='in', res=300)
 ggarrange(scatter, int, slope, ncol=1, nrow=3, labels=c("a)", "b)", "c)"))
 dev.off()
 
@@ -383,6 +445,7 @@ dev.off()
 # make a combined plot
 library(ggpubr)
 
-png("era-specific PDO - Papa and salinity.png", 6.5, 5, units='in', res=300)
+png("figs/era-specific PDO - Papa and salinity.png", 6.5, 5, units='in', res=300)
 ggarrange(scatter, int, ncol=1, nrow=2, labels=c("a)", "b)"))
 dev.off()
+
