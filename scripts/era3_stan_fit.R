64888123c3b4c6cd58bcf05832987407d475409c
mike.litzow <mike.litzow@noaa.gov>
2020-08-12 14:21:52 -0800

    Cleaning up

--- scripts/era3_stan_fit.R
+++ scripts/era3_stan_fit.R
@@ -7,8 +7,8 @@ library(rstanarm)
 library(bayesplot)
 library(overlapping)
 
+# load data
 # NOTE THAT THESE YEARS ARE ALREADY LAGGED TO ENTRY YEAR
-# raw.dat <- read.csv("salmon.and.covariate.data.csv")
 raw.dat <- read.csv("data/salmon.and.SWFSC.PDO.csv")
 
 # re-lagging to catch year to make this consistent across spp. -
@@ -18,17 +18,18 @@ raw.dat$catch.year <- ifelse(raw.dat$species=="Sockeye", raw.dat$Year+2, raw.dat
 raw.dat[["era"]] <- ifelse(raw.dat$catch.year <= 1988, "era1",
                            ifelse(raw.dat$catch.year %in% 1989:2013, "era2", "era3"))
 
+# define color palette for plotting
 cb <- c("#999999", "#E69F00", "#56B4E9", "#009E73",
         "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+
+# set theme for plotting
+theme_set(theme_bw())
 
 
 ## Prep data -----------------------------------------------
 dat3 <- na.omit(raw.dat)
 dat3 <- plyr::ddply(dat3, .(species), transform, pdo = scale(PDO3))
 dat3 <- plyr::ddply(dat3, .(species), transform, sst = scale(SST3))
-
-# dat3 <- plyr::ddply(dat3, .(species), transform, sst = (9/5)*SST3+32) # changing to raw ºF!
-
 dat3$era <- as.factor(dat3$era)
 
 ## Find start and end indices for each species
@@ -50,13 +51,8 @@ dat3_stan <- list(y = dat3$catch,
                   era2 = ifelse(dat3$catch.year %in% 1989:2013, 1, 0),
                   era3 = ifelse(dat3$catch.year >= 2014, 1, 0))
 
-## Catch distribution
-g <- ggplot(dat3) +
-  aes(x = species, y = catch) +
-  geom_boxplot() +
-  theme_bw()
-print(g)
 
+# order species for plotting
 
 dat3$plot.order <- ifelse(dat3$species=="Pink-odd", 1,
                           ifelse(dat3$species=="Pink-even", 2,
@@ -64,14 +60,10 @@ dat3$plot.order <- ifelse(dat3$species=="Pink-odd", 1,
 
 dat3$species <- reorder(dat3$species, dat3$plot.order)
 
-
-# set colors
-cb <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
-
 # reset era names for plotting
 dat3$era.labs <- factor(dat3$era, labels = c("1965-1988", "1989-2013", "2014-2019"))
 
-## Catch vs. PDO
+## scatter plot - catch vs. PDO
 ## This suggests to me that we could pool all species
 scatter <- ggplot(dat3) +
   aes(x = pdo, y = catch, color = species) +
@@ -84,7 +76,7 @@ scatter <- ggplot(dat3) +
 
 print(scatter)
 
-
+# now the era-specific estimate of catch-PDO relationship
 ## 3 era: hierarchical --> model for manuscript ----------------
 era3_hier_arm <- stan_glmer(catch ~ era + pdo + pdo:era + (era + pdo + pdo:era | species),
                             data = dat3,
@@ -98,20 +90,27 @@ era3_hier_arm <- stan_glmer(catch ~ era + pdo + pdo:era + (era + pdo + pdo:era |
                                                      concentration = 1,
                                                      shape = 1, scale = 1))
 
-# do predictions for recent era
-newdata = expand.grid("era"=unique(dat3$era),
-                      pdo=c(mean(dat3$pdo[which(dat3$era=="era3")]),
-                            mean(dat3$pdo[which(dat3$era=="era3")])+1), species=unique(dat3$species)) %>%
-  dplyr::filter(era=="era3")
-pred = posterior_predict(era3_hier_arm, newdata=newdata)
-newdata$mean_pred = round(apply(pred,2,mean), 3)
-
+# examine model output
 fixef(era3_hier_arm)
 ranef(era3_hier_arm)
 coef(era3_hier_arm)
 era3_hier_arm$covmat
 print(era3_hier_arm)
 
+
+## Diagnostics
+posterior <- as.array(era3_hier_arm)
+mcmc_rhat(rhat(era3_hier_arm))
+mcmc_neff(neff_ratio(era3_hier_arm))
+mcmc_trace(posterior)
+ppc_dens_overlay(y = dat3$catch, yrep = posterior_predict(era3_hier_arm, draws = 100))
+mcmc_areas(posterior,
+           pars = c("pdo", "eraera2:pdo", "eraera3:pdo"),
+           prob = 0.95)
+range(summary(era3_hier_arm[["stanfit"]])[["summary"]][ , "n_eff"])
+range(summary(era3_hier_arm[["stanfit"]])[["summary"]][ , "Rhat"])
+
+# combine posteriors for plotting
 mu_beta  <- as.matrix(era3_hier_arm, pars = c("pdo", "eraera2:pdo", "eraera3:pdo"))
 coef_beta <- data.frame(coef = "Slope",
                         era1 = mu_beta[ , 1],
@@ -150,11 +149,11 @@ slope_overlap = overlapping::overlap(x = list(slope1 = mu_beta[,1],slope2=mu_bet
 int_overlap = overlapping::overlap(x = list(int1 = mu_alpha[ , 1],int2=mu_alpha[ , 2],int3=mu_alpha[ , 3]))
 
 
-
-
 saveRDS(int_overlap$OV,file="output/salmon_int_overlap.rds")
 saveRDS(slope_overlap$OV,file="output/salmon_slope_overlap.rds")
 
+
+# and plot
 slopes <- ggplot(mbeta, aes(x = value, fill = variable)) +
   theme_bw() +
   geom_density(alpha = 0.7) +
@@ -176,18 +175,6 @@ dev.off()
 tiff("figs/era-specific catches and 3-yr PDO.tiff", 8, 3, units='in', res=300)
 ggpubr::ggarrange(scatter, slopes, ncol=2, nrow=1, labels=c("a)", "b)"), widths = c(1, 0.7), label.y = 0.95)
 dev.off()
-
-## Diagnostics
-posterior <- as.array(era3_hier_arm)
-mcmc_rhat(rhat(era3_hier_arm))
-mcmc_neff(neff_ratio(era3_hier_arm))
-mcmc_trace(posterior)
-ppc_dens_overlay(y = dat3$catch, yrep = posterior_predict(era3_hier_arm, draws = 100))
-mcmc_areas(posterior,
-           pars = c("pdo", "eraera2:pdo", "eraera3:pdo"),
-           prob = 0.95)
-range(summary(era3_hier_arm[["stanfit"]])[["summary"]][ , "n_eff"])
-range(summary(era3_hier_arm[["stanfit"]])[["summary"]][ , "Rhat"])
 
 
 ## Plot slope and intercepts
